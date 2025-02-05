935daa4440edb0b439a1656820523e1aba6c5d95
teauk03 <kto031124@gamil.com>
2025-02-05 16:12:25 +0900

    Spring @Bean 설정 📚

--- study/src/main/java/com/study01/HelloWorldConfiguration.java
+++ study/src/main/java/com/study01/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.study01;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) { }
+record Person(String name, int age, Address address) { }
 // 주소를 추가 해볼거임 - 첫 행, 도시
 record Address(String firstLine , String city) { }
 @Configuration
@@ -22,11 +22,32 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("Riva",23);
+        return new Person("Riva",23, new Address("567","Busan"));
+    }
+
+   // 기존 Spring Bean과 관계가 있는 새로운 Bean을 생성 - person2
+    @Bean
+    public Person person2MethodeCall() {
+        return new Person(name(), age(), address()); // name, age
+            // 이 문제를 해결하는 방법 1. 메서드 호출
+    }
+
+    // 기존 Spring Bean과 관계가 있는 새로운 Bean을 생성 - person3
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) //  name, age, address3
+    {
+        return new Person(name, age, address3); // name, age
+            // 이 문제를 해결하는 방법 2. 매개변수를 활용
     }
     
-    @Bean
+    
+    @Bean(name = "address2")
     public Address address() {
         return new Address("123","Seoul");
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("789","Suwon");
     }
 }
